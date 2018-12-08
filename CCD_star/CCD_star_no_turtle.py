38f78026629bc62c754876c099f8559f137ec42b
F3RR1N <37646764+F3RR1N@users.noreply.github.com>
2018-12-08 16:28:06 -0700

    Update and rename CCD_star.py to CCD_star_no_turtle.py

--- CCD_star/CCD_star_no_turtle.py
+++ CCD_star/CCD_star_no_turtle.py
@@ -1,5 +1,18 @@
+
+'''
+RBE550 Fall 2018 Total Coverage Team Project
+WPI MS Robotics engineering
+Ryan Ferrin, Karthick Dhavamani, Krishna Matta
+2018/Dec/09
+1 of 3 algorithms tested for complete Coverage
+
+This is an Implementation of Complete Coverage D* algorithm described by
+Marija Dakulović ∗ Sanja Horvatić ∗ Ivan Petrović. 2011
+
+implimentation by Ryan Ferrin 2018
+'''
+
 from PIL import Image
-import turtle
 import numpy
 import queue as Q
 import time
@@ -14,20 +27,20 @@ yellow = (255, 255, 100)
 
 # map deficullty
 # easiest
-pic1 = "test.gif"
-pic2 = "test_2.gif"
-pic3 = "test_3.gif"
-pic4 = "test_4.gif"
+# pic1 = "test.gif"
+# pic2 = "test_2.gif"
+# pic3 = "test_3.gif"
+# pic4 = "test_4.gif"
 
 # easy - few obstacles
 # pic1 = "test3.gif"
 # pic2 = "test3_2.gif"
 
 # # easy - many obstacles
-# pic1 = "Store.gif"
-# pic2 = "Store2.gif"
-# pic3 = "Store3.gif"
-# pic4 = "Store4.gif"
+pic1 = "Store.gif"
+pic2 = "Store2.gif"
+pic3 = "Store3.gif"
+pic4 = "Store4.gif"
 
 #Medium
 # pic1 = "test2.gif"
@@ -109,7 +122,8 @@ def sort(x, y, end, path, grid, neighbors, columns, rows, front):
         return
     # check for obstacle
     elif grid[x, y] == 1:
-        return
+        if (x, y) in front:
+            front.remove((x, y))
     # Check if path clear
     elif grid[x, y] == 0:
         h = cost(x, y, end)
@@ -292,6 +306,46 @@ clean = [] # a list of where we've been
 
 count = 1
 output1 = mapcount(count)
+grid1 = output1[0]
+columns = output1[1]
+rows = output1[2]
+im1 = output1[3]
+
+dirty = ccd_star_plan(start, end, front, grid1, columns, rows, im1)
+dirty1 = dirty
+
+count = 2
+output2 = mapcount(count)
+grid = output2[0]
+im = output2[3]
+spot = 0
+i=0
+
+while spot != end:
+    spot = dirty[i]
+    clean.append(spot)
+    if spot == end:
+         break
+    #look ahead
+    ahead = dirty[i + 1]
+    if grid[ahead[0], ahead[1]] == 1:
+        # front = nob_front(clean, dirty1)
+        print(len(front))
+        dirty = ccd_star_plan(spot, end, front, grid, columns, rows, im)
+        count = count + 1
+        output = mapcount(count)
+        grid = output[0]
+        im = output[3]
+        i = 0
+    else:
+        i = i + 1
+
+runstop = time.time()
+print('Final Path Length ' + str(len(clean)))
+print('TOTAL EXECUTION TIME FOR Planning: ' + str(runstop - runstart))
+
+dupe = overlapped(clean)
+visualize_search(im1, dirty1, dupe)
 grid1 = output1[0]
 columns = output1[1]
 rows = output1[2]
